34abbc74919332ce462fe10ff4ebd48df092bf24
Xine75 <cjdoza@gmail.com>
2020-12-05 19:42:01 -0600

    officer select added

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,6 +1,7 @@
 import { getCriminals, useCriminals } from "./CriminalDataProvider.js"
 import { Criminal } from "./CriminalComponent.js"
 import { useConvictions } from "../convictions/ConvictionProvider.js"
+import { useOfficers } from "../officers/OfficerProvider.js"
 
 //Defines where eventHub broadcasts from
 const eventHub = document.querySelector(".container")
@@ -46,6 +47,35 @@ eventHub.addEventListener("crimeChosen", event => {
         render(matchingCriminals)
   } 
 }) 
+
+//Listen for the customEvent you dispatched in OfficerSelect
+eventHub.addEventListener("officerSelect", event => {
+
+  //Use the property you added to the event detail.
+  if (event.detail.officerThatWasChosen !== "0"){
+
+    //Filter the officers application state downt the people who were arrested by that officer
+
+    console.log("officer", event.detail)
+
+    //Invoke useOfficers() to access array of officers
+    const officers = useOfficers()
+
+    //Finds the id number associated with the officer (and stores it in a new var)
+    const officer = officers.find( (officer) => criminal.id === parseInt(event.detail.officerThatWasChosen) )
+
+    //Invoke useCriminals() to access the array of criminals (and store it in a new var)
+    const criminals = useCriminals()
+
+    //Filters criminals by checking if their arresting officer matches the officer name
+    const arrestedCriminals = criminalElement.filter( (criminal) => criminal.arrestingOfficer === officer.name)
+
+    //Calls render on arrestedCriminals
+    render(arrestedCriminals)
+  }
+})
+
+
 //Renders all the criminals to the DOM after getting them from the API
 export const CriminalList = ()=> {
 getCriminals().then(() => {
