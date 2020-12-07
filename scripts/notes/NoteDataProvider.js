6ccd27c4078c67b6251ca42944c6f503cd70c52d
Xine75 <cjdoza@gmail.com>
2020-12-07 15:03:29 -0600

    added notes fields

---
+++ scripts/notes/NoteDataProvider.js
@@ -0,0 +1,35 @@
+//NoteDataProvider fetches notes and posts notes to our API
+//it creates a function called saveNote taht allows user input
+//to be saved via json to the API
+
+const eventHub = document.querySelector(".container")
+let notes = []
+
+//listens for ???
+const dispatchStateChangeEvent = () => {
+    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+
+    eventHub.dispatchEvent(noteStateChangedEvent)
+}
+
+const getNotes = () => {
+    return fetch('http://localhost:8088/notes')
+        .then(response => response.json())
+        .then(parsedNotes => {
+            notes = parsedNotes
+        })
+
+}
+
+export const saveNote = note => {
+    return fetch('http://localhost:8088/notes', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        //strigifies the note object
+        body: JSON.stringify(note)
+    })
+    .then(getNotes)
+    .then(dispatchStateChangeEvent)
+}
